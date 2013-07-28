42ba843a89470f19ab91b6226d755618f61af13e
Tomasz Kowalczyk <tomasz.kowalczyk@gmail.com>
2013-07-27 23:33:43 -0400

    Removed the sandbox garbage

--- sandbox/MathFunctions/MathFunctions.h
+++
@@ -1,6 +0,0 @@
-#ifndef MATH_FUNCTIONS_H
-#define MATH_FUNCTIONS_H
-
-double mysqrt (double x) throw ();
-
-#endif
--- sandbox/MathFunctions/mysqrt.cpp
+++
@@ -1,6 +0,0 @@
-
-// Custom implementation of the sqrt function
-double mysqrt (double x) throw ()
-{
-	return 2.0 * x;
-}
